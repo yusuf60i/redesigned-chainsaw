9988c29c1fd45331c0d635645dff074c9a201e77
yusuf60i <157643332+yusuf60i@users.noreply.github.com>
2026-03-03 20:52:00 +0300

    Create script.js with functions for button interactions

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Function to handle "No" button click
+function moveNo() {
+    // Add your logic for "No" button interaction
+    console.log('No button clicked!');
+}
+
+// Function to handle "Yes" button click
+function yesClick() {
+    // Add your logic for "Yes" button interaction
+    console.log('Yes button clicked!');
+}
